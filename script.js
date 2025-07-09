5381d68587c51776787c3efec9b1daef9fc3abf9
olekwa <johnolekwa@gmal.com>
2025-07-09 15:23:19 +0100

    upated javescript with copy to clipboard method

--- script.js
+++ script.js
@@ -10,6 +10,36 @@ fadeRange.addEventListener('input', () => {
 
 generateBtn.addEventListener('click', generatePalette);
 
+paletteContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('copy-btn')){
+    const hexValue = e.target.previousElementSibling.textContent;
+
+    navigator.clipboard
+      .writeText(hexValue)
+      .then(() => showCopySuccess(e.target))
+      .catch(alert(err));
+  } else if (e.target.classList.contains('color')) {
+    const hexValue = e.target.nextElementSibling.querySelector('.hex-value').textContent;
+    navigator.clipboard
+      .writeText(hexValue)
+      .then(() => showCopySuccess(e.target .nextElementSibling.querySelector('.copy-btn')))
+      .catch(alert(err));
+  }
+});
+
+function showCopySuccess(element) {
+  element.classList.remove('far', 'fa-copy');
+  element.classList.add('fas', 'fa-check');
+  element.style.color = '#48bb78';
+
+
+  setTimeout(() => {
+    element.classList.remove('fas', 'fa-check');
+    element.classList.add('far', 'fa-copy');
+    element.style.color = '';
+  }, 1500);
+}
+
 function generatePalette() {
   const fadeLevel = parseInt(fadeRange.value); // 0 to 100
   const baseHue = Math.floor(Math.random() * 360);
